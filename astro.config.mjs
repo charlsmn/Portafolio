92e5ca4f1155fb354f4eebf6a359f328414e19b5
Charls <carlos.mancilla993@gmail.com>
2024-05-09 10:37:49 -0500

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import db from '@astrojs/db'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import db from '@astrojs/db';
+import icon from 'astro-icon';
+import node from '@astrojs/node';
 
-import node from '@astrojs/node'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), db(), icon()],
-    output: 'server',
-    adapter: node({
-        mode: 'standalone',
-    }),
-})
+  integrations: [tailwind(), db(), icon()],
+  output: 'server',
+  adapter: vercel()
+});
