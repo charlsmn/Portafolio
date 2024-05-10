fe5b9445fe0173fcf6fe41c656b6d7e783ed88c6
Charls <carlos.mancilla993@gmail.com>
2024-05-10 12:03:44 -0500

    server hybrid test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import db from '@astrojs/db';
-import icon from 'astro-icon';
-import node from '@astrojs/node';
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import db from '@astrojs/db'
+import icon from 'astro-icon'
+import node from '@astrojs/node'
+import vercel from '@astrojs/vercel/serverless'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), icon(), react()],
-  output: 'server',
-  adapter: vercel()
-});
+    integrations: [tailwind(), db(), icon(), react()],
+    output: 'hybrid',
+    adapter: vercel(),
+})
