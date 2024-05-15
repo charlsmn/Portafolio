3961067d9d109834d15974fdc57a192b29324e85
Charls <carlos.mancilla993@gmail.com>
2024-05-15 09:23:32 -0500

    de todo

---
+++ src/components/MouseAnimated.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import AnimatedCursor from 'react-animated-cursor'
+
+export const MouseAnimated = () => {
+    return (
+        <AnimatedCursor
+            innerSize={10}
+            outerSize={20}
+            color="255, 51, 102"
+            outerAlpha={0.1}
+            innerScale={0.7}
+            outerScale={3}
+            outerStyle={{
+                border: '2px solid rgb(255, 51, 102)',
+            }}
+            clickables={[
+                'a',
+                'input[type="text"]',
+                'input[type="email"]',
+                'input[type="number"]',
+                'input[type="submit"]',
+                'input[type="image"]',
+                'label[for]',
+                'select',
+                'textarea',
+                'button',
+                '.link',
+            ]}
+        />
+    )
+}
---
+++ src/content/works/pwa-la-salle.md
@@ -0,0 +1,12 @@
+---
+title: La Salle Mobile Application
+description: 'Tuve algunos problemas, pero preguntar en la comunidad me ayudó mucho.'
+client: La Salle
+image:
+    url: '@assets/images/la-salle-1.png'
+    alt: 'La Salle mobile application'
+pubDate: 2022-07-15
+category: ['Web Development', 'PWA', 'UI Design']
+---
+
+asdasdasd
